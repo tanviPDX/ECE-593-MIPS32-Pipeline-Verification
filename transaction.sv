3b9d9290f9d154063f2adf258cefc08bf4f52272
23deekshaa <93370621+23deekshaa@users.noreply.github.com>
2022-05-20 21:50:47 -0700

    Update transaction.sv

--- transaction.sv
+++ transaction.sv
@@ -7,6 +7,19 @@ class mips22;
 	bit [`WORD_LEN-1:0] WB_result;
 	bit hazard_detected;
 	bit [`WORD_LEN-1:0] PC_MEM; 
+	
+constraint opcode { foreach (instr_byte[i]) {
+			if (i % 4 == 3){
+                        (( instr_byte[i] & 252) >> 2) inside {0, 1, 3, 5, 6, 7, 
+				8, 9, 10, 11, 12, 32, 33, 36, 37, 40, 41, 42 };	  
+		  			}
+		  }}
+
+constraint imm_field { foreach (instr_byte[i]) {
+		        if (i % 4 == 3){
+                            if ((( instr_byte[i] & 252) >> 2) > 36) {  
+				      instr_byte[i-2] == 8'b0; }
+		     }}}
 endclass
 	
 	
